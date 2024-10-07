7fb78d86369e9e13a1e340942fed97700d4d329e
FatiVenturino <fati1.ven@gmail.com>
2024-10-07 12:17:17 -0300

    Listeners

--- src/main/python/dhs2024/App.py
+++ src/main/python/dhs2024/App.py
@@ -2,11 +2,12 @@ import sys
 from antlr4 import *
 from compiladoresLexer  import compiladoresLexer
 from compiladoresParser import compiladoresParser
-
+from Escucha import Escucha
 
 def main(argv):
     # archivo = "input/entrada.txt"
     # archivo = "input/parentesis.txt"
+    # archivo = "input/programa.txt"
     archivo = "input/opal.txt"
     if len(argv) > 1 :
         archivo = argv[1]
@@ -14,8 +15,10 @@ def main(argv):
     lexer = compiladoresLexer(input)
     stream = CommonTokenStream(lexer)
     parser = compiladoresParser(stream)
+    escucha = Escucha()
+    parser.addParseListener(escucha)
     tree = parser.programa()
-    print(tree.toStringTree(recog=parser))
+    # print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
